7ac83857a51895b211923525c768ba3f068b6c41
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-27 19:07:19 +0200

    starting working on matala

--- src/ex4_java_client/StudentCode.java
+++ src/ex4_java_client/StudentCode.java
@@ -1,11 +1,28 @@
-package ex4_java_client; /**
+package ex4_java_client;
+
+ /**
  * @author AchiyaZigi
  * A trivial example for starting the server and running all needed commands
  */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class StudentCode {
+    private void moveGraphTOJson(String graph){
+        try{
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            FileWriter fw = new FileWriter("to_load.json");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
     public static void main(String[] args) {
         Client client = new Client();
         try {
@@ -14,33 +31,34 @@ public class StudentCode {
             e.printStackTrace();
         }
         String graphStr = client.getGraph();
+
         System.out.println(graphStr);
-        client.addAgent("{\"id\":0}");
-        String agentsStr = client.getAgents();
-        System.out.println(agentsStr);
-        String pokemonsStr = client.getPokemons();
-        System.out.println(pokemonsStr);
-        String isRunningStr = client.isRunning();
-        System.out.println(isRunningStr);
-
-        client.start();
-        int counter = 0;
-
-        while (client.isRunning().equals("true")) {
-            client.move();
-            System.out.println(client.getAgents());
-            System.out.println(client.timeToEnd());
-
-            Scanner keyboard = new Scanner(System.in);
-            System.out.println("enter the next dest: ");
-            int next = keyboard.nextInt();
-            client.chooseNextEdge("{\"agent_id\":0, \"next_node_id\": " + next + "}");
-            if(counter == 3)
-            {
-                client.stop();
-            }
-            counter++;
-        }
+//        client.addAgent("{\"id\":0}");
+//        String agentsStr = client.getAgents();
+//        System.out.println(agentsStr);
+//        String pokemonsStr = client.getPokemons();
+//        System.out.println(pokemonsStr);
+//        String isRunningStr = client.isRunning();
+//        System.out.println(isRunningStr);
+//
+//        client.start();
+//        int counter = 0;
+//
+//        while (client.isRunning().equals("true")) {
+//            client.move();
+//            System.out.println(client.getAgents());
+//            System.out.println(client.timeToEnd());
+//
+//            Scanner keyboard = new Scanner(System.in);
+//            System.out.println("enter the next dest: ");
+//            int next = keyboard.nextInt();
+//            client.chooseNextEdge("{\"agent_id\":0, \"next_node_id\": " + next + "}");
+//            if(counter == 3)
+//            {
+//                client.stop();
+//            }
+//            counter++;
+//        }
     }
 
 }
