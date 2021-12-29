69c7d04abf500210a1c7ed09095e02471c38eebd
Talia Seada <78349342+TaliaSeada@users.noreply.github.com>
2021-12-29 18:58:39 +0200

    Delete src directory

--- src/code/Main.java
+++
@@ -1,9 +0,0 @@
-package code;
-
-public class Main {
-    public static void main(String[] args) {
-        Ex2.getGrapgAlgo(args[0]);
-        Ex2.getGrapg(args[0]);
-//        Ex2.runGUI(args[0]);
-    }
-}
