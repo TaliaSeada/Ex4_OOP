e75d6a1aa2ff8abd2cc246e6933762215a92d790
LiorBreitman8234 <liorbreitman321@gmail.com>
2022-01-03 20:42:27 +0200

    changed all simpleNamespace to dictionary

--- Client/student_code.py
+++ Client/student_code.py
@@ -34,7 +34,7 @@ client = Client()
 client.start_connection(HOST, PORT)
 
 pokemons = client.get_pokemons()
-pokemons_obj = json.loads(pokemons, object_hook=lambda d: SimpleNamespace(**d))
+pokemons_obj = json.loads(pokemons)
 
 print(pokemons)
 
@@ -42,10 +42,6 @@ graph_json = client.get_graph()
 
 FONT = pygame.font.SysFont('Arial', 20, bold=True)
 # load the json string into SimpleNamespace Object
-
-graph = json.loads(
-    graph_json, object_hook=lambda json_dict: SimpleNamespace(**json_dict))
-
 print(client.get_info())
 first_split = client.get_info().split(',')
 print(first_split)
@@ -56,15 +52,18 @@ algo = GraphAlgo()
 algo.load_from_json("../" + path)
 center, dist = algo.centerPoint()
 
-for n in graph.Nodes:
-    x, y, _ = n.pos.split(',')
-    n.pos = SimpleNamespace(x=float(x), y=float(y))
+graph = json.loads(graph_json)
+print(graph)
+for n in graph["Nodes"]:
+    print(n["pos"])
+    x, y, _ = n["pos"].split(',')
+    n["pos"] = SimpleNamespace(x=float(x), y=float(y))
 
 # get data proportions
-min_x = min(list(graph.Nodes), key=lambda n: n.pos.x).pos.x
-min_y = min(list(graph.Nodes), key=lambda n: n.pos.y).pos.y
-max_x = max(list(graph.Nodes), key=lambda n: n.pos.x).pos.x
-max_y = max(list(graph.Nodes), key=lambda n: n.pos.y).pos.y
+min_x = min(list(graph["Nodes"]), key=lambda n: n["pos"].x)["pos"].x
+min_y = min(list(graph["Nodes"]), key=lambda n: n["pos"].y)["pos"].y
+max_x = max(list(graph["Nodes"]), key=lambda n: n["pos"].x)["pos"].x
+max_y = max(list(graph["Nodes"]), key=lambda n: n["pos"].y)["pos"].y
 
 
 def scale(data, min_screen, max_screen, min_data, max_data):
@@ -102,18 +101,19 @@ The implementation below should be improved significantly:
 The GUI and the "algo" are mixed - refactoring using MVC design pattern is required.
 """
 game = Game(algo)
-pokemonsFirst = json.loads(client.get_pokemons(),
-                           object_hook=lambda d: SimpleNamespace(**d)).Pokemons
-pokemonsFirst = [p.Pokemon for p in pokemonsFirst]
-for p in pokemonsFirst:
-    pikachu = Pokemon(p.value, p.type, p.pos)
+pokemonsFirst = json.loads(client.get_pokemons())
+print(pokemonsFirst)
+for p in pokemonsFirst["Pokemons"]:
+    p = p["Pokemon"]
+    print(p)
+    pikachu = Pokemon(p["value"], p["type"], p["pos"])
     game.pokemons.append(pikachu)
 agentsPath = []
 game.setPokemonsEdges()
 
-agentsFirst = json.loads(client.get_agents(),
-                         object_hook=lambda d: SimpleNamespace(**d)).Agents
-agentsFirst = [agent.Agent for agent in agentsFirst]
+agentsFirst = json.loads(client.get_agents())
+print(agentsFirst)
+agentsFirst = [agent["Agent"] for agent in agentsFirst["Agents"]]
 
 for a in agentsFirst:
     agentsPath.append([])
@@ -124,15 +124,15 @@ for p in game.pokemons:
     id = 0
     bestid = 0
     for a in agentsFirst:
-        id = a.id
-        if len(agentsPath[a.id]) == 0:
-            dist, path = game.graph.shortest_path(a.src, p.on.getSrcNode())
+        id = a["id"]
+        if len(agentsPath[id]) == 0:
+            dist, path = game.graph.shortest_path(a["src"], p.on.getSrcNode())
             bestid = id
             bestPath = path
             break
         else:
-            dist, path = game.graph.shortest_path(agentsPath[a.id][-1], p.on.getSrcNode())
-            path2, dist2 = game.graph.TSP(agentsPath[a.id])
+            dist, path = game.graph.shortest_path(agentsPath[id][-1], p.on.getSrcNode())
+            path2, dist2 = game.graph.TSP(agentsPath[id])
             if dist+dist2 < bestDist:
                 bestDist = dist+dist2
                 bestid = id
@@ -142,16 +142,15 @@ for p in game.pokemons:
     agentsPath[bestid].append(p.on.getDestNode())
 
 while client.is_running() == 'true':
-    pokemons = json.loads(client.get_pokemons(),
-                          object_hook=lambda d: SimpleNamespace(**d)).Pokemons
-    pokemons = [p.Pokemon for p in pokemons]
+    pokemons = json.loads(client.get_pokemons())
+    pokemons = [p["Pokemon"] for p in pokemons["Pokemons"]]
     currPokemons = []
 
     for p in pokemons:
-        pikachu = Pokemon(p.value, p.type, p.pos)
+        pikachu = Pokemon(p["value"], p["type"], p["pos"])
         currPokemons.append(pikachu)
-        x, y, _ = p.pos.split(',')
-        p.pos = SimpleNamespace(x=my_scale(
+        x, y, _ = p["pos"].split(',')
+        p["pos"] = SimpleNamespace(x=my_scale(
             float(x), x=True), y=my_scale(float(y), y=True))
 
     #
@@ -167,9 +166,8 @@ while client.is_running() == 'true':
 
     game.setPokemonsEdges()
 
-    agents = json.loads(client.get_agents(),
-                        object_hook=lambda d: SimpleNamespace(**d)).Agents
-    agents = [agent.Agent for agent in agents]
+    agents = json.loads(client.get_agents())
+    agents = [agent["Agent"] for agent in agents["Agents"]]
     # set the new Pokemon's path
     for p in new_pokemons:
         bestDist = float('inf')
@@ -177,29 +175,29 @@ while client.is_running() == 'true':
         id = 0
         bestid = 0
         for a in agents:
-            if p.on.getSrcNode() == a.src and p.on.getDestNode() == a.dest:
+            if p.on.getSrcNode() == a["src"] and p.on.getDestNode() == a["dest"]:
                 continue
-            id = a.id
-            if len(agentsPath[a.id]) == 0:
-                if a.dest != -1:
-                    dist, path = game.graph.shortest_path(a.dest, p.on.getSrcNode())
+            id = a["id"]
+            if len(agentsPath[id]) == 0:
+                if a["dest"] != -1:
+                    dist, path = game.graph.shortest_path(a["dest"], p.on.getSrcNode())
                     # path.pop(0)
                 else:
-                    dist, path = game.graph.shortest_path(a.src, p.on.getSrcNode())
+                    dist, path = game.graph.shortest_path(a["src"], p.on.getSrcNode())
                     # path.pop(0)
                 bestPath = path
                 break
             else:
-                if a.dest != -1:
-                    dist, path = game.graph.shortest_path(a.dest, p.on.getSrcNode())
+                if a["dest"] != -1:
+                    dist, path = game.graph.shortest_path(a["dest"], p.on.getSrcNode())
                     # path.pop(0)
                 else:
-                    dist, path = game.graph.shortest_path(a.src, p.on.getSrcNode())
+                    dist, path = game.graph.shortest_path(a["src"], p.on.getSrcNode())
                     # path.pop(0)
 
                 if dist > bestDist:
                     continue
-                path2, dist2 = game.graph.TSP(agentsPath[a.id])
+                path2, dist2 = game.graph.TSP(agentsPath[a["id"]])
                 if dist2 > bestDist:
                     continue
                 if dist + dist2 < bestDist:
@@ -211,11 +209,11 @@ while client.is_running() == 'true':
         agentsPath[bestid].append(p.on.getDestNode())
 
     for a in agents:
-        agentsPath[a.id] = [x[0] for x in groupby(agentsPath[a.id])]
+        agentsPath[a["id"]] = [x[0] for x in groupby(agentsPath[a["id"]])]
 
     for a in agents:
-        x, y, _ = a.pos.split(',')
-        a.pos = SimpleNamespace(x=my_scale(
+        x, y, _ = a["pos"].split(',')
+        a["pos"] = SimpleNamespace(x=my_scale(
             float(x), x=True), y=my_scale(float(y), y=True))
     # check events
     events = pygame.event.get()
@@ -234,11 +232,10 @@ while client.is_running() == 'true':
 
     # refresh surface
     screen.fill(Color(200, 200, 200))
-    print(agents)
     # draw nodes
-    for n in graph.Nodes:
-        x = my_scale(n.pos.x, x=True)
-        y = my_scale(n.pos.y, y=True)
+    for n in graph["Nodes"]:
+        x = my_scale(n["pos"].x, x=True)
+        y = my_scale(n["pos"].y, y=True)
 
         # its just to get a nice antialiased circle
 
@@ -248,21 +245,21 @@ while client.is_running() == 'true':
                          radius, Color(255, 255, 255))
 
         # draw the node id
-        id_srf = FONT.render(str(n.id), True, Color(255, 255, 255))
+        id_srf = FONT.render(str(n["id"]), True, Color(255, 255, 255))
         rect = id_srf.get_rect(center=(x, y))
         screen.blit(id_srf, rect)
 
     # draw edges
-    for e in graph.Edges:
+    for e in graph["Edges"]:
         # find the edge nodes
-        src = next(n for n in graph.Nodes if n.id == e.src)
-        dest = next(n for n in graph.Nodes if n.id == e.dest)
+        src = next(n for n in graph["Nodes"] if n["id"] == e["src"])
+        dest = next(n for n in graph["Nodes"] if n["id"] == e["dest"])
 
         # scaled positions
-        src_x = my_scale(src.pos.x, x=True)
-        src_y = my_scale(src.pos.y, y=True)
-        dest_x = my_scale(dest.pos.x, x=True)
-        dest_y = my_scale(dest.pos.y, y=True)
+        src_x = my_scale(src["pos"].x, x=True)
+        src_y = my_scale(src["pos"].y, y=True)
+        dest_x = my_scale(dest["pos"].x, x=True)
+        dest_y = my_scale(dest["pos"].y, y=True)
 
         # draw the line
         pygame.draw.line(screen, Color(61, 72, 126),
@@ -271,37 +268,37 @@ while client.is_running() == 'true':
     # draw agents
     for agent in agents:
         pygame.draw.circle(screen, Color(150, 150, 200),
-                           (int(agent.pos.x), int(agent.pos.y)), 10)
-        id_srf = FONT.render(str(agent.id), True, Color(0, 0, 0))
-        rect = id_srf.get_rect(center=(int(agent.pos.x), int(agent.pos.y)))
+                           (int(agent["pos"].x), int(agent["pos"].y)), 10)
+        id_srf = FONT.render(str(agent["id"]), True, Color(0, 0, 0))
+        rect = id_srf.get_rect(center=(int(agent["pos"].x), int(agent["pos"].y)))
         screen.blit(id_srf, rect)
 
     # draw pokemons
     for p in pokemons:
-        if p.type == 1:
-            pygame.draw.circle(screen, Color(203, 108, 200), (int(p.pos.x), int(p.pos.y)), 10)
+        if p["type"] == 1:
+            pygame.draw.circle(screen, Color(203, 108, 200), (int(p["pos"].x), int(p["pos"].y)), 10)
         else:
-            pygame.draw.circle(screen, Color(203, 108, 0), (int(p.pos.x), int(p.pos.y)), 10)
+            pygame.draw.circle(screen, Color(203, 108, 0), (int(p["pos"].x), int(p["pos"].y)), 10)
 
     # update screen changes
     pygame_widgets.update(events)
     display.update()
 
     # refresh rate
-    clock.tick(70)
-    prevDest = [agent.dest for agent in agents]
+    clock.tick(60)
+    prevDest = [agent["dest"] for agent in agents]
 
     # choose next edge
     for agent in agents:
-        if agent.dest == -1:
-            print(agentsPath[agent.id])
-            if len(agentsPath[agent.id]) == 0:
+        if agent["dest"] == -1:
+            print(agentsPath[agent["id"]])
+            if len(agentsPath[agent["id"]]) == 0:
                 continue
-            next_node = agentsPath[agent.id].pop(0)
-            while agent.src == next_node:
-                next_node = agentsPath[agent.id].pop(0)
+            next_node = agentsPath[agent["id"]].pop(0)
+            while agent["src"] == next_node:
+                next_node = agentsPath[agent["id"]].pop(0)
             client.choose_next_edge(
-                '{"agent_id":' + str(agent.id) + ', "next_node_id":' + str(next_node) + '}')
+                '{"agent_id":' + str(agent["id"]) + ', "next_node_id":' + str(next_node) + '}')
             ttl = client.time_to_end()
             print(ttl, client.get_info())
             #print(agent)
